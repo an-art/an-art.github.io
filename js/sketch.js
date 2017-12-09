155c7fba328d0c84c6571a748b6a33ee35c0f4c7
Polina Bobrovnik <polinabobrovnik@gmail.com>
2017-12-09 19:46:50 +0300

    Add field rendering

--- js/sketch.js
+++ js/sketch.js
@@ -8,16 +8,29 @@ function setup() {
     let clientWidth = document.documentElement.clientWidth;
 
     cnvs = createCanvas(500, 500);
-
     cnvs.style('background: url("") rgb(51, 0, 0)');
     cnvs.position(clientWidth/2-250, 50);
-    for (let i = 0; i < 400; i++) {
-        field.push(new Tile(i % 20, Math.floor(i / 20), "BISCUIT"));
-    }
+
+    field = generateField();
 }
 
 function draw() {
     for (let i = 0; i < field.length; i++) {
         field[i].draw();
     }
+}
+
+function generateField() {
+    let f = [];
+
+    for (let i = 0; i < FIELD_lvl_1.length; i++) {
+        let row = FIELD_lvl_1[i].split(',');
+
+        for (let j = 0; j < row.length; j++) {
+            let type = parseTileType(row[j]);
+            let t = new Tile(i, j, type);
+            f.push(t);
+        }
+    }
+    return f;
 }
